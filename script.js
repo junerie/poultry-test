0883a203b234f2f9ce56e15f229c5496748156db
junerie <yjunerie@gmail.com>
2023-09-26 17:15:09 +0800

    add content

--- script.js
+++ script.js
@@ -11,6 +11,14 @@ const nav2 = document.getElementById('nav-2');
 const nav3 = document.getElementById('nav-3');
 const nav4 = document.getElementById('nav-4');
 const nav5 = document.getElementById('nav-5');
+const Home = document.getElementById('home');
+const about = document.getElementById('about');
+const skills = document.getElementById('skills');
+const projects = document.getElementById('projects');
+const contact = document.getElementById('contact');
+const navItems = [nav1, nav2, nav3, nav4, nav5];
+const sectionItems = [Home, skills, projects, contact];
+
 
 let apiQuotes = [];
 
@@ -25,6 +33,16 @@ function complete() {
     quoteContainer.hidden = false;
     loader.hidden = true;
 }
+
+// Control navigation Animation
+function navAnimation(direction1, direction2) {
+    navItems.forEach((nav, i) => {
+        nav.classList.replace(`slide-${direction1}-${i + 1}`,`slide-${direction2}-${i + 1}`);
+    });
+}
+
+
+
 // toggle nav
 function toggleNav(){
     // toggle menubars open/close
@@ -33,36 +51,17 @@ function toggleNav(){
     overlay.classList.toggle('overlay-active');
     if (overlay.classList.contains('overlay-active')) {
       // animate in - overlay
-        overlay.classList.remove('overlay-slide-left');
-        overlay.classList.add('overlay-slide-right');
+        overlay.classList.replace('overlay-slide-left', 'overlay-slide-right');
         // animate In - nav item
-        nav1.classList.remove('slide-out-1');
-        nav1.classList.add('slide-in-1');
-        nav2.classList.remove('slide-out-2');
-        nav2.classList.add('slide-in-2');
-        nav3.classList.remove('slide-out-3');
-        nav3.classList.add('slide-in-3');
-        nav4.classList.remove('slide-out-4');
-        nav4.classList.add('slide-in-4');
-        nav5.classList.remove('slide-out-5');
-        nav5.classList.add('slide-in-5');
+        navAnimation('out', 'in')
     } else {
         // animate out -overlay
-        overlay.classList.remove('overlay-slide-right');
-        overlay.classList.add('overlay-slide-left');
+        overlay.classList.replace('overlay-slide-right', 'overlay-slide-left');
         // animate out - nav item
-        nav1.classList.remove('slide-in-1');
-        nav1.classList.add('slide-out-1');
-        nav2.classList.remove('slide-in-2');
-        nav2.classList.add('slide-out-2');
-        nav3.classList.remove('slide-in-3');
-        nav3.classList.add('slide-out-3');
-        nav4.classList.remove('slide-in-4');
-        nav4.classList.add('slide-out-4');
-        nav5.classList.remove('slide-in-5');
-        nav5.classList.add('slide-out-5');
+        navAnimation('in', 'out')
     }
 }
+
 // Show new Quote 
 function newQuotes() {
     loading();
@@ -100,15 +99,46 @@ function tweetQuotes() {
     window.open(twitterUrl, '_blank');
 }
 
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+
 // Event Listener
 newquoteBtn.addEventListener('click', newQuotes);
 twitterBtn.addEventListener('click', tweetQuotes);
 menuBars.addEventListener('click', toggleNav);
-nav1.addEventListener('click', toggleNav);
-nav2.addEventListener('click', toggleNav);
-nav3.addEventListener('click', toggleNav);
-nav4.addEventListener('click', toggleNav);
-nav5.addEventListener('click', toggleNav); 
+navItems.forEach((nav) => {
+nav.addEventListener('click', toggleNav);
+});
+
+
+
 
 
 
